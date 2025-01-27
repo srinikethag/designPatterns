a74acad4b3decd24d56f87e515512ffcaedd2f13
Sriniketha Geejagar <sriniketha.geejagar@toggl.com>
2025-01-27 21:07:26 +0530

    Registry along with Prototype

--- src/main/java/prototype/SigningConfig/Client.java
+++ src/main/java/prototype/SigningConfig/Client.java
@@ -3,11 +3,18 @@ package prototype.SigningConfig;
 public class Client {
     public static void main(String[] args) {
         SigningConfig obj1 = new CloudSigningConfig("key1", "cert1", "url");
+        SigningConfig obj2 = new SigningConfig("key2", "cert2");
 
-        SigningConfig copy = createCopy(obj1);
+        Registry registry = new Registry();
+        registry.add(Registry.RegistryType.CLOUD, obj1);
+        registry.add(Registry.RegistryType.SIGNING, obj2);
+
+        SigningConfig copy = createCopy(registry.get(Registry.RegistryType.CLOUD));
+        SigningConfig copy2 = createCopy(registry.get(Registry.RegistryType.SIGNING));
 
         System.out.println(copy);
         System.out.println(obj1);
+        System.out.println(copy2);
     }
 
     private static SigningConfig createCopy(SigningConfig sc){
---
+++ src/main/java/prototype/SigningConfig/Registry.java
@@ -0,0 +1,20 @@
+package prototype.SigningConfig;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Registry {
+    public enum RegistryType{
+        SIGNING, CLOUD, LOCAL
+    }
+
+    private Map<RegistryType, SigningConfig> mp = new HashMap<>();
+
+    public SigningConfig get(RegistryType type){
+        return mp.get(type);
+    }
+
+    public void add(RegistryType type, SigningConfig sc){
+        mp.put(type, sc);
+    }
+}
