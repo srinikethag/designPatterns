baa0cd64ef60d0229ece9e69fe017088ca0b3e13
Sriniketha Geejagar <sriniketha.geejagar@toggl.com>
2025-02-04 10:48:39 +0530

    Slot finder example of Strategy design pattern

---
+++ src/main/java/Strategy/SlotFinder/OffPeakHoursStrategy.java
@@ -0,0 +1,10 @@
+package Strategy.SlotFinder;
+
+public class OffPeakHoursStrategy implements SlotFinderStrategy{
+    @Override
+    public Slot findSlot(Vehicle vehicle) {
+
+        // nearest slot possible allocate
+        return new Slot();
+    }
+}
---
+++ src/main/java/Strategy/SlotFinder/PriorityStrategy.java
@@ -0,0 +1,10 @@
+package Strategy.SlotFinder;
+
+public class PriorityStrategy implements SlotFinderStrategy{
+
+    @Override
+    public Slot findSlot(Vehicle vehicle) {
+        // find slot on priority
+        return new Slot();
+    }
+}
